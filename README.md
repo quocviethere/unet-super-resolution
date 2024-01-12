c44f02e08524985406ae6459d9967cc729b9a72c
quocviethere <quocviethere@gmail.com>
2024-01-12 11:29:04 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# domain-conversion
+# Super Resolution
+
+ This project aims to implement a Unet-like model that performs the super resolution task of receiving an input image of 64x64 size and outputting an image of the same content with the size increases by 4 times (256x256). The dataset we use can be downloaded [here](https://drive.google.com/file/d/17NiVpVxpkvbc2WDvz1VP0sos6NdbPjLE/view?usp=sharing). 
+
+ 
+
--- model.py
+++ model.py
@@ -2,12 +2,125 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 
-
 LOW_IMG_HEIGHT = 64
 LOW_IMG_WIDTH = 64
 
+class FirstFeatureNoSkip(nn.Module):
+    '''
+    Implementation of UNET without Skip Connection
+    '''
+    def __init__(self, in_channels, out_channels):
+        super(FirstFeatureNoSkip, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias=False),
+            nn.LeakyReLU()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class ConvBlockNoSkip(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlockNoSkip, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class EncoderNoSkip(nn.Module):
+    def __init__(self, in_channels, out_channels) -> None:
+        super(EncoderNoSkip, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.MaxPool2d(2),
+            ConvBlockNoSkip(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        return x
+
+class DecoderNoSkip(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DecoderNoSkip, self).__init__()
+        self.conv = nn.Sequential(
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(in_channels, out_channels*2, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_channels*2),
+            nn.LeakyReLU(),
+        )
+        self.conv_block = ConvBlockNoSkip(out_channels*2, out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.conv_block(x)
+        return x
+
+class FinalOutputNoSkip(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(FinalOutputNoSkip, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, 1, 0, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class SR_Unet_NoSkip(nn.Module):
+    def __init__(
+            self, n_channels=3, n_classes=3
+    ):
+        super(SR_Unet_NoSkip, self).__init__()
+
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.resize_fnc = transforms.Resize((LOW_IMG_HEIGHT*4, LOW_IMG_HEIGHT*4),
+                                             antialias=True)
+        self.in_conv1 = FirstFeatureNoSkip(n_channels, 64)
+        self.in_conv2 = ConvBlockNoSkip(64, 64)
+
+        self.enc_1 = EncoderNoSkip(64, 128)
+        self.enc_2 = EncoderNoSkip(128, 256)
+        self.enc_3 = EncoderNoSkip(256, 512)
+        self.enc_4 = EncoderNoSkip(512, 1024)
+
+        self.dec_1 = DecoderNoSkip(1024, 512)
+        self.dec_2 = DecoderNoSkip(512, 256)
+        self.dec_3 = DecoderNoSkip(256, 128)
+        self.dec_4 = DecoderNoSkip(128, 64)
+
+        self.out_conv = FinalOutputNoSkip(64, n_classes)
+
+
+    def forward(self, x):
+        x = self.resize_fnc(x)
+        x = self.in_conv1(x)
+        x = self.in_conv2(x)
+
+        x = self.enc_1(x)
+        x = self.enc_2(x)
+        x = self.enc_3(x)
+        x = self.enc_4(x)
+
+        x = self.dec_1(x)
+        x = self.dec_2(x)
+        x = self.dec_3(x)
+        x = self.dec_4(x)
+
+        x = self.out_conv(x)
+        return x
 
 class FirstFeature(nn.Module):
+    '''
+    Implementation of UNET with Skip connections
+    '''
     def __init__(self, in_channels, out_channels):
         super(FirstFeature, self).__init__()
         self.conv = nn.Sequential(
@@ -17,7 +130,6 @@ class FirstFeature(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -34,7 +146,6 @@ class ConvBlock(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-
 class Encoder(nn.Module):
     def __init__(self, in_channels, out_channels) -> None:
         super().__init__()
@@ -46,7 +157,6 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         return x
-
 
 class Decoder(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -65,7 +175,6 @@ class Decoder(nn.Module):
         x = self.conv_block(x)
         return x
 
-
 class FinalOutput(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(FinalOutput, self).__init__()
@@ -76,7 +185,6 @@ class FinalOutput(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-
 
 class SR_Unet(nn.Module):
     def __init__(
@@ -103,7 +211,6 @@ class SR_Unet(nn.Module):
 
         self.out_conv = FinalOutput(64, n_classes)
 
-
     def forward(self, x):
         x = self.resize_fnc(x)
         x = self.in_conv1(x)
@@ -121,8 +228,8 @@ class SR_Unet(nn.Module):
 
         x = self.out_conv(x)
         return x
- 
-
-unet_model = SR_Unet()    
-img = torch.ones(2, 3, 256, 256)
-unet_model(img).shape
+    
+# Example usage
+# unet_model = SR_Unet()    
+# img = torch.ones(2, 3, 256, 256)
+# unet_model(img).shape
